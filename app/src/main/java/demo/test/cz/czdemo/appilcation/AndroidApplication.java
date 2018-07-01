4970105fc8c0f9f171890b86ea25d30715de513d
chenzhen <812000108@qq.com>
2018-07-01 13:21:58 +0800

    feat:添加application

---
+++ app/src/main/java/demo/test/cz/czdemo/appilcation/AndroidApplication.java
@@ -0,0 +1,14 @@
+package demo.test.cz.czdemo.appilcation;
+
+import android.app.Application;
+
+/**
+ * @description:
+ * @FileName: AndroidApplication.java
+ * @author: chenzhen
+ * @date: 2018-07-01
+ * @e-mail:18814806231@163.com
+ */
+
+public class AndroidApplication extends Application {
+}
